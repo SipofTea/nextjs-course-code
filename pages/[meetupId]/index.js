8fb4c32c3b5e27689c8e901f4a6fc9a41b0fef42
Else Meijerink <p.e.meijerink@gmail.com>
2021-12-03 10:26:52 +0000

    Added detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,7 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+ return <MeetupDetail image="https://upload.wikimedia.org/wikipedia/commons/9/92/NDU-Goldkuppel.jpg" title="A first meetup" address="1 Suffolk St, London SW1Y 4HG" description="This is the first meetup"></MeetupDetail>
+}
+
+export default MeetupDetails;
